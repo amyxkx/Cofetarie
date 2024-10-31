49add29e9e2daacc3f982eb55ab5ab7750766949
Amalia Gr <amyama.grebanus@gmail.com>
2024-10-31 11:02:27 +0200

    Tema1

--- main.cpp
+++ main.cpp
@@ -14,13 +14,10 @@ class Ornament {
     float price; // per bucata
 
 public:
-    Ornament(std::string  n, float amnt, float p ) : name(std::move(n)), amount(amnt), price( p ) {
+    Ornament(std::string  n, float amnt, float p ) : name(std::move(n)), amount(amnt), price(p) {}
 
-    }
-    Ornament(const Ornament& other)
-        : name(other.name), amount(other.amount), price(other.price) {}
+    Ornament(const Ornament& other) : name(other.name), amount(other.amount), price(other.price) {}
 
-    // Copy Assignment Operator
     Ornament& operator=(const Ornament& other) {
         if (this != &other) {
             name = other.name;
@@ -29,9 +26,11 @@ public:
         }
         return *this;
     }
-    [[nodiscard]] std::string getName() const {
+
+    [[nodiscard]] const std::string& getName() const& {
         return name;
     }
+
     [[nodiscard]] float getAmount() const {
         return amount;
     }
@@ -40,9 +39,7 @@ public:
         return price;
     }
 
-    ~Ornament() {
-        //std::cout<<"Ornament destructor"<< std::endl;
-    }
+    ~Ornament() {}
 
     friend std::ostream& operator<<(std::ostream& os, const Ornament& ornm) {
         os << ornm.name << " (" << ornm.amount << " grams)";
@@ -58,64 +55,69 @@ class CakeDetails {
     std::vector<Ornament> ornaments;
     int tiers;
     int days_until_expiration;
-    float weight_without_ornaments; //in kg
+    float weight_without_ornaments;
     float price_per_kg;
     bool sugarfree;
     bool glutenfree;
 
 public:
-    CakeDetails(const std::string &cknm, const std::string& flv, std::string  cr, const std::string& msg,
+    CakeDetails(const std::string &cknm, const std::string& flv, std::string cr, const std::string& msg,
              const std::vector<Ornament>& ornm, int t, int days, float w, float price , bool nosugar, bool nogluten)
     : CakeName(cknm), flavor(flv), cream(std::move(cr)), message(msg), ornaments(ornm), tiers(t),
-      days_until_expiration(days), weight_without_ornaments(w), price_per_kg(price), sugarfree(nosugar), glutenfree(nogluten) { };
+      days_until_expiration(days), weight_without_ornaments(w), price_per_kg(price), sugarfree(nosugar), glutenfree(nogluten) {}
 
+    // [[nodiscard]] bool isSugarfree() const {
+    //     return sugarfree;
+    // }
+    //
+    // [[nodiscard]] bool isGlutenfree() const {
+    //     return glutenfree;
+    // }
 
-    [[nodiscard]] bool isSugarfree() const  {
-        return sugarfree;
-    }
-    [[nodiscard]] bool isGlutenfree() const  {
-        return glutenfree;
-    }
+    // [[nodiscard]] const std::string& getCakeName() const {
+    //     return CakeName;
+    // }
 
-    std::string getCakeName() {
-        return CakeName;
-    }
+    // [[nodiscard]] const std::string& getFlavor() const {
+    //     return flavor;
+    // }
 
-    std::string getFlavor() {
-        return flavor;
-    }
-    std::string getCream() {
-        return cream;
-    }
+    // [[nodiscard]] const std::string& getCream() const {
+    //     return cream;
+    // }
+
     [[nodiscard]] int getTiers() const {
         return tiers;
     }
-    [[nodiscard]] float getWeight() const {
-        return weight_without_ornaments;
-    }
-    [[nodiscard]] float getPricePerKg() const {
-        return price_per_kg;
-    }
-    std::string getMessage()  {
-        return message;
-    }
-    [[nodiscard]] int getDaysUntilExpiration() const {
-        return days_until_expiration;
-    }
-    std::vector<Ornament>& getOrnaments()  {
-        return ornaments;
-    }
 
+    // [[nodiscard]] float getWeight() const {
+    //     return weight_without_ornaments;
+    // }
+
+    // [[nodiscard]] float getPricePerKg() const {
+    //     return price_per_kg;
+    // }
+
+    // [[nodiscard]] const std::string& getMessage() const {
+    //     return message;
+    // }
+
+    // [[nodiscard]] int getDaysUntilExpiration() const {
+    //     return days_until_expiration;
+    // }
+
+    // const std::vector<Ornament>& getOrnaments() const {
+    //     return ornaments;
+    // }
 
     void displaySummary() const {
-
-        std::cout << "Flavor of the cake base: "     << flavor << std::endl;
-        std::cout << "Number of tiers: "             << tiers << std::endl;
+        std::cout << "Flavor of the cake base: " << flavor << std::endl;
+        std::cout << "Number of tiers: " << tiers << std::endl;
         std::cout << "Weight of the cake (edible): " << weight_without_ornaments << " grams" << std::endl;
-        std::cout << "Covering cream: "              << cream << std::endl;
-        std::cout << "Price per kg: "                << price_per_kg << std::endl;
+        std::cout << "Covering cream: " << cream << std::endl;
+        std::cout << "Price per kg: " << price_per_kg << std::endl;
         std::cout << "Ornaments (on top of cake): ";
-        for (auto& ornm : ornaments) {
+        for (const auto& ornm : ornaments) {
             std::cout << ornm.getName() << " ";
         }
         std::cout << std::endl;
@@ -123,20 +125,19 @@ public:
 
     void TotalWeight() const {
         float total_weight = this->weight_without_ornaments;
-        for( auto& ornm : ornaments ) {
-            total_weight+=ornm.getAmount();
+        for (const auto& ornm : ornaments) {
+            total_weight += ornm.getAmount();
         }
-        std::cout<<"Total weight for the cake is: "<<total_weight<<std::endl;
+        std::cout << "Total weight for the cake is: " << total_weight << std::endl;
     }
 
     void CostOfCake() const {
         float price = this->weight_without_ornaments * this->price_per_kg;
-        for (auto& ornm : ornaments) {
+        for (const auto& ornm : ornaments) {
             price += ornm.getPrice();
         }
         std::cout << "Cost of Cake with ornaments: " << price << std::endl;
     }
-
 };
 
 class Order {
@@ -147,23 +148,35 @@ class Order {
     std::string payment;
 
 public:
-    Order(const CakeDetails& ck, std::string Delivery, std::string pay) : cake(ck), DateOfDelivery(std::move(Delivery)), payment(std::move(pay)) {
+    Order(const CakeDetails& ck, std::string Delivery, std::string pay)
+    : cake(ck), DateOfDelivery(std::move(Delivery)), payment(std::move(pay)) {
         orderID = ++orderCounter;
     }
 
-    [[nodiscard]] int getOrderID() const { return orderID; }
-    [[nodiscard]] const CakeDetails& getCakeDetails() const { return cake; }
-    [[nodiscard]] const std::string& getDateOfDelivery() const { return DateOfDelivery; }  // Const reference added
-    [[nodiscard]] const std::string& getPayment() const { return payment; }                // Const reference added
+    [[nodiscard]] int getOrderID() const {
+        return orderID;
+    }
+
+    [[nodiscard]] const CakeDetails& getCakeDetails() const {
+        return cake;
+    }
+
+    [[nodiscard]] const std::string& getDateOfDelivery() const {
+        return DateOfDelivery;
+    }
+
+    [[nodiscard]] const std::string& getPayment() const {
+        return payment;
+    }
 
     ~Order() {
         std::cout << "Order destructor for number: " << orderID << std::endl;
     }
 
     friend std::ostream& operator<<(std::ostream& os, const Order& order){
-        os << "Order ID: " << order.getOrderID() << " Cake: " ;
+        os << "Order ID: " << order.getOrderID() << " Cake: ";
         order.getCakeDetails().displaySummary();
-        os<<" Delivery date: "<<order.getDateOfDelivery()<<" Payment method: "<< order.getPayment()<<std::endl;
+        os << " Delivery date: " << order.getDateOfDelivery() << " Payment method: " << order.getPayment() << std::endl;
         return os;
     }
 };
@@ -177,26 +190,31 @@ private:
     Order order;
 
 public:
-    Client(std::string nm, std::string phone, std::string adrs, std::string email, const Order &ord ) : name(std::move(nm)), phoneNumber(std::move( phone)), OrderAddress(std::move(adrs)), EmailAddress(std::move(email)), order( ord ) {}
+    Client(std::string nm, std::string phone, std::string adrs, std::string email, const Order &ord)
+    : name(std::move(nm)), phoneNumber(std::move(phone)), OrderAddress(std::move(adrs)), EmailAddress(std::move(email)), order(ord) {}
 
-    std::string getName()  {
+    [[nodiscard]] const std::string& getName() const {
         return name;
     }
-    std::string getEmailAddress(){
-        return EmailAddress;
-    }
-    std::string getPhoneNumber()  {
-        return phoneNumber;
-    }
-    std::string getAddress()  {
-        return OrderAddress;
-    }
-    Order getOrder()  {
+
+    // [[nodiscard]] const std::string& getEmailAddress() const {
+    //     return EmailAddress;
+    // }
+
+    // [[nodiscard]] const std::string& getPhoneNumber() const {
+    //     return phoneNumber;
+    // }
+
+    // [[nodiscard]] const std::string& getAddress() const {
+    //     return OrderAddress;
+    // }
+
+    [[nodiscard]] const Order& getOrder() const {
         return order;
     }
 
     friend std::ostream& operator<<(std::ostream& os, const Client& cust) {
-        os << "Customer: " << cust.name << ", Phone: " << cust.phoneNumber<<" Email: "<< cust.EmailAddress<<" Address: "<< cust.OrderAddress<<std::endl;
+        os << "Customer: " << cust.name << ", Phone: " << cust.phoneNumber << " Email: " << cust.EmailAddress << " Address: " << cust.OrderAddress << std::endl;
         return os;
     }
 };
@@ -220,7 +238,9 @@ void displayWelcomeMessage(  ) {
         } else {
             word += caracter; }
     }
-    std::cout << word << "                     *\n"; std::cout << "*                "; word = "";
+    std::cout << word << "                     *\n";
+    std::cout << "*                ";
+    word = "";
 
     for (char caracter : line2) {
         if (caracter == ' ') {
@@ -307,9 +327,9 @@ int main() {
 
      clearScreen();
 
-     Ornament ornm1("Mrs&Mr Figurine", 0.3, 100);
-     Ornament ornm2("Boy/Girl Figurine", 0.3, 20 );
-     Ornament ornm4("Animated Character Figurine", 0.1, 30);
+     Ornament ornm1("Mrs&Mr Figurine", 0.3f, 100);
+     Ornament ornm2("Boy/Girl Figurine", 0.3f, 20 );
+     Ornament ornm4("Animated Character Figurine", 0.1f, 30);
 
      std::vector<Ornament> birthdayOrnaments= { ornm2, ornm4 };
      std::vector<Ornament> weddingOrnaments= { ornm1 };
@@ -320,21 +340,21 @@ int main() {
 
      std::vector<CakeDetails> cakes = {
         CakeDetails("Chocolate Birthday Monster", "Chocolate", "Chocolate Ganache", "Happy Birthday (name and age)!",
-            birthdayOrnaments, 1, 5, 2, 100, false, false),
+            birthdayOrnaments, 1, 5, 2.0f, 100, false, false),
         CakeDetails("Vanilla Birthday Lover", "Vanilla", "Buttercream", "Happy Birthday (name and age)!",
-            birthdayOrnaments, 1, 5, 2, 100, false, false),
+            birthdayOrnaments, 1, 5, 2.0f, 100, false, false),
         CakeDetails("Vanilla Wedding Dream", "Vanilla", "White Chocolate Ganache", "For a lifetime of happiness Mrs&Mr",
-            weddingOrnaments, 3, 3, 10, 150, false, false),
+            weddingOrnaments, 3, 3, 10.0f, 150, false, false),
         CakeDetails("Carrot Cake", "Carrot", "Cream Cheese", "Don't forget to smile!",
-            carrotOrnaments, 1, 6, 1.5, 80, false, true),
+            carrotOrnaments, 1, 6, 1.5f, 80, false, true),
         CakeDetails("Classic Cheesecake Sugar&Gluten Free", "Biscuit Base", "Cream Cheese", "Have a beautiful day",
-            cheesecakeOrnaments, 1, 5, 1.5, 80, true, true),
+            cheesecakeOrnaments, 1, 5, 1.5f, 80, true, true),
         CakeDetails("Forest Fruit Cheesecake Sugar Free", "Biscuit Base", "Cream Cheese", "Have a beautiful day",
-            cheesecakeOrnaments, 1, 5, 1.5, 90, true, false),
+            cheesecakeOrnaments, 1, 5, 1.5f, 90, true, false),
         CakeDetails("Lemon Graduation Cake Sugar&Gluten free", "Vanilla with Lemon", "Lemon Frosting", "Happy Graduation!",
-            lemonOrnaments, 1, 4, 2, 90, true, true),
+            lemonOrnaments, 1, 4, 2.0f, 90, true, true),
         CakeDetails("Congratulation Special: Red Velvet", "Red Velvet", "Cream Cheese", "Congratulations!",
-            redVelvetOrnaments, 2, 5, 3, 130, false, false)
+            redVelvetOrnaments, 2, 5, 3.0f, 130, false, false)
     };
 
     displayOrderMessage( cakes );
